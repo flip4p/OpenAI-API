0ca1edef0747f5820c411cde5ac494b84dba4cd0
fifigd <filip.porebski1999@gmail.com>
2024-11-21 12:21:03 +0100

    Implement extract_txt function to read content from a file

--- main2.py
+++ main2.py
@@ -4,3 +4,9 @@ import password
 
 
 openai=OpenAI(api_key=password.key)
+
+
+def extract_txt():
+    with open("Zadanie_dla_JJunior_AI_Developera_tresc_artykulu.txt", "r", encoding="UTF-8") as file:
+        content = file.read()
+        return content
